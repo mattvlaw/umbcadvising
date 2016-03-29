f0ce5c491ab017a6efff0168cbaa44626d007137
AdamOstrove <ostrove1@umbc.edu>
2016-03-28 22:56:01 -0400

    Added files via upload

---
+++ classList.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf8">
+	<title>Computer Science Class List</title>
+</head>
+<body>
+	<!-- credit counter -->
+	<div id="credcounter"></div>
+
+	<form action="" id="classlist">
+		<!-- class path tree -->
+		<div id="classtree"> 
+
+			<!-- computer science path trees -->
+			<div id="compsci">
+				<div id="cs200"> 
+					<!-- CMSC 200 level courses -->
+					<input type="checkbox" class="" name="course[]" value="cmsc201">CMSC 201
+					<input type="checkbox" class="" name="course[]" value="cmsc202">CMSC 202
+					<input type="checkbox" class="" name="course[]" value="cmsc203">CMSC 203
+					<input type="checkbox" class="" name="course[]" value="cmsc232">CMSC 232
+					<input type="checkbox" class="" name="course[]" value="cmsc291">CMSC 291
+					<input type="checkbox" class="" name="course[]" value="cmsc299">CMSC 299
+				</div>
+				<br>
+				<div id="cs300"> 
+					<!-- CMSC 300 level courses --> 
+					<input type="checkbox" class="" name="course[]" value="cmsc304">CMSC 304
+					<input type="checkbox" class="" name="course[]" value="cmsc313">CMSC 313
+					<input type="checkbox" class="" name="course[]" value="cmsc331">CMSC 331
+					<input type="checkbox" class="" name="course[]" value="cmsc341">CMSC 341
+					<input type="checkbox" class="" name="course[]" value="cmsc352">CMSC 352
+					<input type="checkbox" class="" name="course[]" value="cmsc391">CMSC 391
+				</div>
+        <br>
+				<div id="cs400"> 
+					<!-- CMSC 400 level courses -->
+				  <input type="checkbox" class="" name="course[]" value="cmsc404">CMSC 403
+          <input type="checkbox" class="" name="course[]" value="cmsc411">CMSC 411
+          <input type="checkbox" class="" name="course[]" value="cmsc421">CMSC 421
+          <input type="checkbox" class="" name="course[]" value="cmsc426">CMSC 426
+          <input type="checkbox" class="" name="course[]" value="cmsc427">CMSC 427
+          <input type="checkbox" class="" name="course[]" value="cmsc431">CMSC 431
+          <input type="checkbox" class="" name="course[]" value="cmsc432">CMSC 432
+          <input type="checkbox" class="" name="course[]" value="cmsc433">CMSC 433
+          <input type="checkbox" class="" name="course[]" value="cmsc435">CMSC 435
+          <input type="checkbox" class="" name="course[]" value="cmsc436">CMSC 436
+          <input type="checkbox" class="" name="course[]" value="cmsc437">CMSC 437
+          <input type="checkbox" class="" name="course[]" value="cmsc441">CMSC 441
+          <input type="checkbox" class="" name="course[]" value="cmsc442">CMSC 442
+          <input type="checkbox" class="" name="course[]" value="cmsc443">CMSC 443
+          <input type="checkbox" class="" name="course[]" value="cmsc446">CMSC 446
+          <input type="checkbox" class="" name="course[]" value="cmsc447">CMSC 447
+          <input type="checkbox" class="" name="course[]" value="cmsc448">CMSC 448
+          <input type="checkbox" class="" name="course[]" value="cmsc451">CMSC 451
+          <input type="checkbox" class="" name="course[]" value="cmsc452">CMSC 452
+          <input type="checkbox" class="" name="course[]" value="cmsc453">CMSC 453
+          <input type="checkbox" class="" name="course[]" value="cmsc455">CMSC 455
+          <input type="checkbox" class="" name="course[]" value="cmsc456">CMSC 456
+          <input type="checkbox" class="" name="course[]" value="cmsc457">CMSC 457
+          <input type="checkbox" class="" name="course[]" value="cmsc461">CMSC 461
+          <input type="checkbox" class="" name="course[]" value="cmsc465">CMSC 465
+          <input type="checkbox" class="" name="course[]" value="cmsc466">CMSC 466
+          <input type="checkbox" class="" name="course[]" value="cmsc471">CMSC 471
+          <input type="checkbox" class="" name="course[]" value="cmsc473">CMSC 473
+          <input type="checkbox" class="" name="course[]" value="cmsc475">CMSC 475
+          <input type="checkbox" class="" name="course[]" value="cmsc476">CMSC 476
+          <input type="checkbox" class="" name="course[]" value="cmsc477">CMSC 477
+          <input type="checkbox" class="" name="course[]" value="cmsc478">CMSC 478
+          <input type="checkbox" class="" name="course[]" value="cmsc479">CMSC 479
+          <input type="checkbox" class="" name="course[]" value="cmsc481">CMSC 483
+          <input type="checkbox" class="" name="course[]" value="cmsc483">CMSC 483
+          <input type="checkbox" class="" name="course[]" value="cmsc484">CMSC 484
+          <input type="checkbox" class="" name="course[]" value="cmsc486">CMSC 486
+          <input type="checkbox" class="" name="course[]" value="cmsc487">CMSC 487
+          <input type="checkbox" class="" name="course[]" value="cmsc491">CMSC 491
+          <input type="checkbox" class="" name="course[]" value="cmsc493">CMSC 493
+          <input type="checkbox" class="" name="course[]" value="cmsc495">CMSC 495
+          <input type="checkbox" class="" name="course[]" value="cmsc498">CMSC 498
+          <input type="checkbox" class="" name="course[]" value="cmsc499">CMSC 499
+				</div>
+			</div>
+			
+			<!-- path trees for the suggested science courses -->
+			<div id="sciences">
+				<div id="path1"> 
+					<!-- chem101 -> chem102 -> chem102L -> GES110 -->  
+				</div>
+				<div id="path2">
+					<!-- chem101 -> chem102 -> biol141 -> any lab -->
+				</div>
+				<div id="path3">
+					<!-- biol 141 -> biol142 -> biol Lab -> phys121 -->
+				</div>
+				<div id="path4">
+					<!-- phys121 -> phsy122 -> ges286 -->
+				</div>
+				<div id="path5">
+					<!-- phys121 -> phys122 -> phys 122L -> math251  -->
+				</div>
+				<div id="path6">
+					<!-- sci -> sci -> ges110/120 -> sci101L -->
+				</div>
+			</div>
+		</div>
+	</form>
+</body>
+</html>
